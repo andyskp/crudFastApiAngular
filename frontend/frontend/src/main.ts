b3e7a89319c4031940ba495243f1edab3f6e0e8d
Andres bustos <effectsxfighter@gmail.com>
2025-05-25 18:02:39 -0500

    Se termina el POST del logueo para rediccionar al todo list

--- frontend/frontend/src/main.ts
+++ frontend/frontend/src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    // hereda los providers de appConfig
+    ...(appConfig.providers ?? []),
+    // añade el provider de HttpClient
+    provideHttpClient(),
+  ],
+})
+.catch((err) => console.error(err));
