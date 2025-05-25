f642f35f22fc0d78d91108f505fdd1d117c9e93b
Andres bustos <effectsxfighter@gmail.com>
2025-05-25 10:26:06 -0500

    Backend funcional

--- backend/app/data.py
+++ backend/app/data.py
@@ -1,5 +1,8 @@
 from .models import User
 
 users_db = {}
+
+default_user = User(username="admin", password="andres1234")
+users_db[default_user.username] = default_user
 tasks_db = []
 task_id_counter = 1
